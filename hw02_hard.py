31043e71e6c44897c2a1790e337030407124d840
kirill0206 <arist84@mail.ru>
2018-11-20 00:06:11 +0300

    home work 2 level hard done

--- hw02_hard.py
+++ hw02_hard.py
@@ -5,6 +5,28 @@ __author__ = 'Aristarkhov Kirill Viktorovich'
 #2. Сколько букв английского алфавита в тексте?
 
 userStr = input("Введите текст: ")
+specSymbol = (",.?!:()/;")
+for symbol in specSymbol:
+    userStr = userStr.replace(symbol, " ")
+
 userList = userStr.split()
+print("В тексте " + str(len(userList)) + " слов")
+
+print(userStr)
+engAlpha = ("abcdefghijklmnopqrstuvwxyz")
+engChar = set()
+for char in userStr:
+    if char in engAlpha:
+        engChar.add(char)
+print(str(len(engChar)) + " букв латинского алфавита")
 
 #Задача-2 Пользователь вводит два текста, необходимо найти все слова, которые есть в обоих текстах. Без учета регистра
+"""
+userAStr = (input("Введите текст: ")).lower()
+userASet = set(userAStr.split())
+
+userBStr = (input("Введите текст: ")).lower()
+userBSet = set(userBStr.split())
+
+resultWords = [word for word in userASet if word in userBSet]
+"""
